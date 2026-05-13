1fab45fa382a6778f901751377d45465b3a635ca
Takeshi FURUTA <furuta@mq-sol.jp>
2026-05-13 21:21:20 +0900

    2nd commit

---
+++ services/common/image.py
@@ -0,0 +1,14 @@
+# standard library
+from pathlib import Path
+
+# third party
+import cv2
+import numpy as np
+
+# local
+from services.common.image import load_rgb
+
+
+def load_rgb(path: Path) -> np.ndarray:
+    rgb = cv2.
+    pass
